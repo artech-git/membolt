ad653e59fae21b08feecf5c08405e8a60f81749f
Prabhat <45896902+artech-git@users.noreply.github.com>
2024-02-19 11:50:32 +0530

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 
 
 mod file; 
-mod sample; 
 
 
 #[cfg(test)]
@@ -10,19 +9,7 @@ mod tests {
 
     use crate::file::DirectIO;
 
-    // use self::sample::DirectIO;
-
     use super::*;
-
-    // #[test]
-    // fn open_file() { 
-    //     const N: usize = 577;
-    //     let mut dio: DirectIO<N> = DirectIO::open("sample.txt").unwrap();
-    //     let s = "kjfadjfsajfjsfjsadfjsdafjsdjfsdf".as_bytes();
-    //     let y = dio.write(s).unwrap();
-    //     dio.flush();
-    //     assert_eq!(s.len(),y);
-    // }
 
     #[test]
     fn develop_file() {
@@ -37,3 +24,6 @@ mod tests {
         assert_eq!(data.len(), y);  
     }
 }
+
+
+use file::*; 
